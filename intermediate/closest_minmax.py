b306a71defbd35e182c7520c75f15590e0157fc5
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-07-13 23:31:40 +0530

    Update closest_minmax.py

--- intermediate/closest_minmax.py
+++ intermediate/closest_minmax.py
@@ -1,27 +1,45 @@
-class Solution:
-    # @param A : list of integers
-    # @return an integer
-    def solve(self, A):
-        N = len(A)
-        min_val = min(A)
-        max_val = max(A)
+# Closest MinMax
 
-        if min_val == max_val:
-            return 1
-        
-        last_min_index = -1
-        last_max_index = -1
-        min_length = N
+# Given an array A, find the size of the smallest subarray such that it
+# contains at least one occurrence of the maximum value of the array
+# and at least one occurrence of the minimum value of the array.
 
-        for i in range(N):
-            if A[i] == min_val:
-                last_min_index = i
-                if last_max_index != -1:
-                    min_length = min(min_length, i - last_max_index + 1)        
-                
-            if A[i] == max_val:
-                last_max_index = i
-                if last_min_index != -1:
-                    min_length = min(min_length, i - last_min_index + 1)
-                       
-        return min_length
+def main():
+  A = [2, 6, 1, 6, 9]
+  print("Smallest Subarray:", end = " ")
+  print(minMax(A))
+
+def minMax(A):
+  N = len(A)
+  min_val = min(A)
+  max_val = max(A)
+
+  # print(f"Min Value: {min_val}. Max Value: {max_val}")
+
+  for i in range(N):
+    if A[i] == min_val:
+      for j in range(i, N):
+        if A[j] == max_val:
+          return j - i + 1
+
+if __name__ == "__main__":
+  main()
+
+
+
+'''
+N = len(A)
+min_val = min(A)
+max_val = max(A)
+min_length = N
+
+for i in range(N):
+        for j in range(i + 1, N):
+            if (A[i] == min_val and A[j] == max_val) or (A[i] == max_val and A[j] == min_val):
+                current_length = j - i + 1
+
+                if current_length < min_length:
+                    min_length = current_length
+
+return min_length
+'''
