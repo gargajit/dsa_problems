289ae77ba8e8711e4e30e89e6b155cfb4414f3b8
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-07-28 23:25:06 +0530

    Create arr_eles_removal_min_cost.py

---
+++ intermediate/arr_eles_removal_min_cost.py
@@ -0,0 +1,17 @@
+# Given an integer array A of size N. You can remove any element from the array in one operation.
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        N = len(A)
+        A.sort()
+        k = 1
+        ans = 0
+
+        for i in range(N-1, -1, -1):
+            ans += A[i] * k
+            k += 1
+        
+        return ans
+
